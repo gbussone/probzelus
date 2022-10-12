65c842623872e6ebad34396a53526d3a2f67f625
Grégoire BUSSONE <gregoire.bussone@ens.fr>
2022-10-12 13:19:26 +0200

    Add the radar

---
+++ bench-apf/radar/particles/lib.ml
@@ -0,0 +1,28 @@
+(*
+ * Copyright 2018-2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+open Owl
+
+let of_lists l = Mat.of_arrays (Array.of_list (List.map Array.of_list l))
+let of_list l n m = Mat.of_array (Array.of_list l) n m
+
+let diagm l = Mat.diagm (of_lists [l])
+let vec l = of_list l (List.length l) 1
+let vec_get x i = Mat.get x i 0
+
+let ( +@ ) m1 m2 = Mat.add m1 m2
+let ( *@ ) x m = Mat.mul_scalar m x
+
